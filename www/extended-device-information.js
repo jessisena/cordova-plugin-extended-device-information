2ed073c1a6b69973cd3ec0351e7a37e98a15561e
Jessica Sena <jessisena@gmail.com>
2018-05-27 23:18:35 +0200

    Comment cordova channel

--- www/extended-device-information.js
+++ www/extended-device-information.js
@@ -1,12 +1,12 @@
 var argscheck = require('cordova/argscheck'),
-    channel = require('cordova/channel'),
-    utils = require('cordova/utils'),
-    exec = require('cordova/exec'),
-    cordova = require('cordova');
+    //channel = require('cordova/channel'),
+    //utils = require('cordova/utils'),
+    exec = require('cordova/exec');
+    // = require('cordova');
 
-channel.createSticky('onCordovaInfoReady');
+//channel.createSticky('onCordovaInfoReady');
 // Tell cordova channel to wait on the CordovaInfoReady event
-channel.waitForInitialization('onCordovaInfoReady');
+//channel.waitForInitialization('onCordovaInfoReady');
 
 /**
  * This represents the mobile device, and provides properties for inspecting the model, version, UUID of the
